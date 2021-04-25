7b3bb79ae7318d21c6706839d93d43f0ebf23c5a
phantranhung <zzsatthupk@gmail.com>
2021-04-25 15:21:18 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const { Keystone } = require('@keystonejs/keystone');
+const { Text } = require('@keystonejs/fields');
+const { GraphQLApp } = require('@keystonejs/app-graphql');
+const { AdminUIApp } = require('@keystonejs/app-admin-ui');
+const { StaticApp } = require('@keystonejs/app-static');
+
+const { MongooseAdapter: Adapter } = require('@keystonejs/adapter-mongoose');
+const PROJECT_NAME = 'my-app';
+const adapterConfig = { mongoUri: 'mongodb+srv://root:Password1998@cluster0.vuqwb.mongodb.net/test-db?authSource=admin&replicaSet=atlas-hhvw1a-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true' };
+
+
+const keystone = new Keystone({
+  adapter: new Adapter(adapterConfig),
+});
+
+keystone.createList('Todo', {
+  schemaDoc: 'A list of things which need to be done',
+  fields: {
+    name: { type: Text, schemaDoc: 'This is the thing you need to do' },
+  },
+});
+
+module.exports = {
+  keystone,
+  apps: [
+    new GraphQLApp(),
+    new StaticApp({ path: '/', src: 'public' }),
+    new AdminUIApp({ name: PROJECT_NAME, enableDefaultRoute: true }),
+  ],
+};
